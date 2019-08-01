bc08a1ffc275cd91318517ff9273bc66a8fa2375
Henry Willams <nthphuc@apcs.vn>
2019-08-01 22:53:17 +0700

    FinalProject

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public GameObject wall;
+    public float wallLength = 10.0f;
+    public int xSize = 10;
+    public int ySize = 10;
+    private Vector3 initialPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateWalls();
+    }
+
+    void CreateWalls()
+    {
+        initialPos = new Vector3((-xSize / 2) + wallLength / 2, 0.0f, (-ySize / 2) + wallLength / 2);
+        Vector3 myPos = initialPos;
+        GameObject tempWall;
+
+        //For x Axis
+        for (int i = 0; i < ySize; i++)
+        {
+            for (int j = 0; j <= xSize; j++)
+            {
+                myPos = new Vector3(initialPos.x + (j * wallLength) - wallLength / 2, 0.0f, initialPos.z + (i * wallLength) - wallLength / 2);
+                Instantiate(wall, myPos, Quaternion.identity);
+            }
+        }
+
+        //For y Axis
+        for (int i = 0; i <= ySize; i++)
+        {
+            for (int j = 0; j < xSize; j++)
+            {
+                myPos = new Vector3(initialPos.x + (j * wallLength) , 0.0f, initialPos.z + (i * wallLength) - wallLength);
+                tempWall = Instantiate(wall, myPos, Quaternion.Euler(0.0f,90.0f,0.0f)) as GameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
